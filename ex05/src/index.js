17acbb2d721d097dab2f124192e8782d442b47cd
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-04 22:46:23 +0200

    Functions - understanding undefined values

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+    //Only change code below this line
+var sum = 0;
+    function addThree(){
+        sum = sum + 3;
+        console.log(sum);
+    }
+addThree();
+
+function addFive(){
+    sum = sum + 5;
+    console.log(sum);
+}
+addFive();
+    //Only change code above this line
+    
+  module.exports = {addThree, addFive};
