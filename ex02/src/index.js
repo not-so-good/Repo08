33f99974e714d4ec7baf04590bd17c447d725769
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-04 22:06:54 +0200

    Functions with arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+    //Only change code below this line
+    function functionPrintInfo(name, age){
+        console.log(name, age);
+    }
+  
+    functionPrintInfo("John", 23);
+    functionPrintInfo("Michael", 43)
+     //Only change code above this line
+  
+  module.exports = functionPrintInfo;
