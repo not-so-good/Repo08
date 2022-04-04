e0eb39b27e4533aba10d03d48259d5ff626cece0
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-04 22:33:50 +0200

    Functions - Global scope

---
+++ ex04/src/index.js
@@ -0,0 +1,21 @@
+//Only change code below this line
+var myGlobalVariable = 10;
+function function1() {
+    myLocalVariable = 5;
+}
+//Only change code above this line
+function function2() {
+    var result = "";
+    if (typeof myGlobalVariable != "undefined") {
+        result += 'myGlobalVariable: ' + myGlobalVariable;
+    }
+    if (typeof myLocalVariable != "undefined") {
+        result += " myLocalVariable: " + myLocalVariable;
+    }
+    console.log(result);
+}
+function1();
+function2();
+
+module.exports = { function1, function2 };
+
