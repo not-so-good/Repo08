9cc2a7e8a651d458c1721569bb4bc1dab513fa8b
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-04 21:51:11 +0200

    Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+    //Only change code below this line
+  function reusableFunction(){
+      console.log("Hi Padawan!");
+  }
+
+  reusableFunction();
+   //Only change code above this line
+
+module.exports = reusableFunction;
