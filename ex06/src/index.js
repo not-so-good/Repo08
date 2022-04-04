ecea4b6ca116248c83388d573b7e0e88d6b21e6e
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-04 23:10:20 +0200

    Functions - Returning a value

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+//Only change code below this line
+function xSix(num) {
+    return num * 6;
+}
+
+//var result = xSix(5);
+//var result = xSix(2);
+var result = xSix(0);
+//Only change code above this line
+
+console.log(result);
+
+module.exports = xSix;
