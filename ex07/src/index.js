528a07752a59325d978f6a6df87e4d42f4fbe9fb
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-04 23:19:49 +0200

    Functions - global vs. local scope

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+//Only change code below this line
+var dress = "little dress";
+function myDress(){
+    var dress = "ball gown";
+//    console.log(dress);
+    return dress;
+}
+//myDress();
+console.log(myDress());
+//Only change code above this line
+module.exports = myDress;
